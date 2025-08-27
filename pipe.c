cafbd99298f5ad52b515be61a1a5a19168eb62a9
DanielRojas1920 <danielrojvilla@gmail.com>
2025-08-26 18:03:18 -0600

    pipe change

--- pipe.c
+++ pipe.c
@@ -8,19 +8,17 @@ int main() {
 
     pipe(fd);     // crear la tubería
 
-    printf("\n%d, %d\n", fd[0], fd[1]);
+    if (fork() == 0) {  //Hijo
+        close(fd[0]);      //Se cierra el extremo de lectura
 
-    if (fork() == 0) {       // aquí entra el HIJO
-        close(fd[0]);        // el hijo no lee, así que cierra el extremo de lectura
-
-        char msg[] = "Hola papá, soy tu hijo";
-        write(fd[1], msg, strlen(msg) + 1);  // escribe en la tubería
+        char msg[] = "Mensaje del hijo";
+        write(fd[1], msg, strlen(msg) + 1);  // Manda el mensaje en el pipe
         close(fd[1]);        // cierra el extremo de escritura
-    } else {                 // aquí entra el PADRE
-        close(fd[1]);        // el padre no escribe, así que cierra el extremo de escritura
+    } else {                 //Padre
+        close(fd[1]);        // Se cierra el extremo de escritura
 
         read(fd[0], buffer, sizeof(buffer)); // lee lo que escribió el hijo
-        printf("El padre recibió: %s\n", buffer);
+        printf("Mensaje recibido: %s\n", buffer);
         close(fd[0]);        // cierra el extremo de lectura
     }
 
