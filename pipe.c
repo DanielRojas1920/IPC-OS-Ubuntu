4bd1a7bb57ca09c63a0cc613b0eb5a3dbe7d5c66
DanielRojas1920 <danielrojvilla@gmail.com>
2025-08-27 11:00:10 -0600

    test pipe

--- pipe.c
+++ pipe.c
@@ -14,16 +14,14 @@ int main() {
 
         char msg[] = "Mensaje del hijo";
         char test[] = " Mensaje 2";
-        write(fd[1], msg, strlen(msg) +1);  // Manda el mensaje en el pipe
+        write(fd[1], msg, strlen(msg));  // Manda el mensaje en el pipe
         write(fd[1], test, strlen(test) + 1);
         close(fd[1]);        // cierra el extremo de escritura
     } else {                 //Padre
         close(fd[1]);        // Se cierra el extremo de escritura
 
         read(fd[0], buffer, sizeof(buffer)); // lee lo que escribió el hijo
-        read(fd[0], buffer_test, sizeof(buffer_test));
         printf("Mensaje recibido: %s\n", buffer);
-        printf("Mensaje recibido: %s\n", buffer_test);
         close(fd[0]);        // cierra el extremo de lectura
     }
 
